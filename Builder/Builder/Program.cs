1a9851fcbf7ef2f91bc755b080ba138b729c12cf
Koleso <fokke_wolf@mail.ru>
2016-02-26 21:59:49 +0300

    1

---
+++ Builder/Builder/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections;
+using System.Runtime.InteropServices;
+
+namespace Builder
+{
+    class Program
+    {
+        static void Main()
+        {
+            Builder builder = new ConcreteBuilder();
+            Director director = new Director(builder);
+
+            director.Construct();
+
+            House house = builder.GetResults();
+        }
+    }
+
+    class Director
+    {
+        Builder builder;
+
+        public Director(Builder builder)
+        {
+            this.builder = builder;
+        }
+
+        public void Construct()
+        {
+            builder.BuildBasement();
+            builder.BuildRoof();
+        }
+    }
+
+    class ConcreteBuilder : Builder
+    {
+        House house = new House();
+
+        public override void BuildBasement()
+        {
+            house.Add(new Basement());
+        }
+
+        public override void BuildRoof()
+        {
+            house.Add(new Roof());
+        }
+
+        public override House GetResults()
+        {
+            return house;
+        }
+    }
+
+    abstract class Builder
+    {
+        public abstract void BuildBasement();
+        public abstract void BuildRoof();
+        public abstract House GetResults();
+    }
+
+    class House
+    {
+        ArrayList parts = new ArrayList();
+
+        public void Add(object part)
+        {
+            parts.Add(part);
+        }
+    }
+
+    class Basement
+    {
+        public Basement()
+        {
+            Console.WriteLine("Фундамент построен");
+        }
+    }
+
+    class Roof
+    {
+        public Roof()
+        {
+            Console.WriteLine("Крыша построена");
+        }
+    }
+}
