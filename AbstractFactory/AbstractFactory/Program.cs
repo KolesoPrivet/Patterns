046bc30463f7ef1c7f652d9f4bafc7e3f063b3e7
Koleso <fokke_wolf@mail.ru>
2016-02-26 19:43:19 +0300

    Simple abstract factory.

---
+++ AbstractFactory/AbstractFactory/Program.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractFactory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Client client = null;
+
+            client = new Client(new CocaColaFactory());
+            client.Run();
+
+            client = new Client(new AquaMineraleFactory());
+            client.Run();
+        }
+    }
+
+    class Client
+    {
+        private AbsWater _water;
+        private AbsBottle _bottle;
+        private AbsLabel _label;
+
+        public Client(AbsFactory factory)
+        {
+            _water = factory.CreateWater();
+            _bottle = factory.CreateBottle();
+            _label = factory.CreateLabel();
+        }
+
+        public void Run()
+        {
+            _bottle.Interact(_water);
+            _bottle.Interact(_label);
+        }
+    }
+
+    abstract class AbsFactory
+    {
+        public abstract AbsBottle CreateBottle();
+        public abstract AbsWater CreateWater();
+        public abstract AbsLabel CreateLabel();
+    }
+
+    abstract class AbsBottle
+    {
+        public abstract void Interact(AbsWater absWater);
+        public abstract void Interact(AbsLabel absLabel);
+    }
+
+    abstract class AbsWater
+    {
+        
+    }
+
+    abstract class AbsLabel
+    {
+        
+    }
+
+    class CocaColaFactory : AbsFactory
+    {
+        public override AbsBottle CreateBottle()
+        {
+            return new CocaColaBottle();
+        }
+
+        public override AbsLabel CreateLabel()
+        {
+            return new CocaColaLabel();
+        }
+
+        public override AbsWater CreateWater()
+        {
+            return new CocaColaWater();
+        }
+    }
+
+    class CocaColaBottle : AbsBottle
+    {
+        public override void Interact(AbsLabel absLabel)
+        {
+            Console.WriteLine(this + " interact with " + absLabel);
+        }
+
+        public override void Interact(AbsWater absWater)
+        {
+            Console.WriteLine(this + " interact with " + absWater);
+        }
+    }
+
+    class CocaColaWater : AbsWater
+    {
+        
+    }
+
+    class CocaColaLabel : AbsLabel
+    {
+         
+    }
+
+    class AquaMineraleFactory : AbsFactory
+    {
+        public override AbsBottle CreateBottle()
+        {
+            return new AquaMineraleBottle();
+        }
+
+        public override AbsLabel CreateLabel()
+        {
+            return new AquaMineraleLabel();
+        }
+
+        public override AbsWater CreateWater()
+        {
+            return new AquaMineraleWarer();
+        }
+    }
+
+    class AquaMineraleBottle : AbsBottle
+    {
+        public override void Interact(AbsLabel absLabel)
+        {
+            Console.WriteLine(this + " interact with " + absLabel);
+        }
+
+        public override void Interact(AbsWater absWater)
+        {
+            Console.WriteLine(this + " interact with " + absWater);
+        }
+    }
+
+    class AquaMineraleWarer : AbsWater
+    {
+        
+    }
+
+    class AquaMineraleLabel : AbsLabel
+    {
+         
+    }
+}
---
+++ AbstractFactory/AbstractFactory/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("AbstractFactory")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AbstractFactory")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("8d9adf5d-b18c-4752-be7f-d5051af72ec3")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
