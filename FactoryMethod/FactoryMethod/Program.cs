a000d684565b30e47a9f9f9f58667c9543bb39ba
Koleso <fokke_wolf@mail.ru>
2016-02-27 17:57:27 +0300

    Factory Method

---
+++ FactoryMethod/FactoryMethod/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Creator creator;
+            Product product;
+
+            creator = new ConcreteCreator();
+            product = creator.FactoryMethod();
+
+            creator.FactoryMethod();
+        }
+    }
+
+    abstract class Creator
+    {
+        private Product product;
+        public abstract Product FactoryMethod();
+
+        public void AnOperation()
+        {
+            product = FactoryMethod();
+        }
+    }
+
+    class ConcreteCreator : Creator
+    {
+        public override Product FactoryMethod()
+        {
+            return new ConcreteProduct();
+        }
+    }
+
+    abstract class Product
+    {
+        
+    }
+
+    class ConcreteProduct : Product
+    {
+        public ConcreteProduct()
+        {
+            Console.WriteLine(GetType());
+        }
+    }
+}
