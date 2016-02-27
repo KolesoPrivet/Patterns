060d4f2dec672e890f82d37d54e579407922d493
Koleso <fokke_wolf@mail.ru>
2016-02-27 18:27:23 +0300

    1

--- FactoryMethod/FactoryMethod/Program.cs
+++ FactoryMethod/FactoryMethod/Program.cs
@@ -1,15 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace FactoryMethod
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             Creator creator;
             Product product;
