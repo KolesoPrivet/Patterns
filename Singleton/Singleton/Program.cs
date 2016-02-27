b4b16f555f7e41dcac6d5bc3b14519204aee0d07
Koleso <fokke_wolf@mail.ru>
2016-02-27 18:57:48 +0300

    1

---
+++ Singleton/Singleton/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton instance = Singleton.getInstance();
+            Singleton instance2 = Singleton.getInstance();
+
+            Console.WriteLine("{0} {1}", instance.GetHashCode(), instance2.GetHashCode());
+        }
+    }
+
+    class Singleton
+    {
+        private static Singleton instance;
+        private static readonly object obj = new object();
+        private Singleton() { }
+
+        public static Singleton getInstance()
+        {
+            if (instance == null) //1st check
+            {
+                lock (obj)
+                {
+                    if(instance == null) //2nd check
+                        instance = new Singleton();
+                }
+            }
+            return instance;
+        }
+    }
+}
