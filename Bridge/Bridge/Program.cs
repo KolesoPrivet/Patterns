f377a6190f80a0eb719ba89be48354677eb3dca1
Koleso <fokke_wolf@mail.ru>
2016-02-28 22:53:42 +0300

    2

---
+++ Bridge/Bridge/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Bridge
+{
+    class Program
+    {
+        static void Main()
+        {
+            Abstraction abstraction = new RefinedAbstraction(new ConcreteImplementor());
+
+            abstraction.Operation();
+        }
+    }
+
+    abstract class Abstraction
+    {
+        private Implementor imp;
+
+        public Abstraction(Implementor imp)
+        {
+            this.imp = imp;
+        }
+
+        public virtual void Operation()
+        {
+            Console.WriteLine("Рисовать фигуру");
+            imp.OperationImp();
+        }
+    }
+
+    class RefinedAbstraction : Abstraction
+    {
+        public RefinedAbstraction(Implementor imp) : base(imp)
+        {
+            
+        }
+
+        public override void Operation()
+        {
+            base.Operation();
+            Console.WriteLine("Рисовать круг");
+        }
+    }
+
+    abstract class Implementor
+    {
+        public abstract void OperationImp();
+    }
+
+    class ConcreteImplementor : Implementor
+    {
+        public override void OperationImp()
+        {
+            Console.WriteLine("Рисовать точками");
+        }
+    }
+}
