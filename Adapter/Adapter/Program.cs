7abb1c3eeca52a0177e27eb8c967e6f6c3658b6d
Koleso <fokke_wolf@mail.ru>
2016-02-28 22:17:49 +0300

    1

---
+++ Adapter/Adapter/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adapter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Adapter adapter = new Adapter();
+            adapter.Request();
+        }
+    }
+
+    interface ITarget
+    {
+        void Request();
+    }
+
+    class Adaptee
+    {
+        public void SpecificRequest()
+        {
+            Console.WriteLine("SpecificRequest");
+        }
+    }
+
+    class Adapter : ITarget
+    {
+        private Adaptee adaptee = new Adaptee();
+        public void Request()
+        {
+            adaptee.SpecificRequest();
+        }
+    }
+
+
+}
