9ce60fbfaa75cbcba774fb9f7fe5d0c24db96ced
Koleso <fokke_wolf@mail.ru>
2016-03-01 20:03:37 +0300

    1

---
+++ Proxy/Proxy/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Proxy
+{
+    class Program
+    {
+        static void Main()
+        {
+            Subject proxy = new Proxy();
+            proxy.Request();
+        }
+    }
+
+    abstract class Subject
+    {
+        public abstract void Request();
+    }
+
+    class RealSubject : Subject
+    {
+        public override void Request()
+        {
+            Console.WriteLine("Real Subject");
+        }
+    }
+
+    class Proxy : Subject
+    {
+        private RealSubject realSubject;
+
+        public override void Request()
+        {
+            if(realSubject == null)
+                realSubject = new RealSubject();
+
+            realSubject.Request();
+        }
+    }
+}
