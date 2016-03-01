28be86e0c77baea5936ea59102f01975b09b2b36
Koleso <fokke_wolf@mail.ru>
2016-03-01 17:40:28 +0300

    1

---
+++ Facade/Facade/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Facade
+{
+    class Program
+    {
+        static void Main()
+        {
+            Facade facade = new Facade();
+
+            facade.OperationA();
+            facade.OperationB();
+        }
+    }
+
+    class Facade
+    {
+        SubSystemA subA = new SubSystemA();
+        SubSystemB subB = new SubSystemB();
+        SubSystemC subC = new SubSystemC();
+
+        public void OperationA()
+        {
+            subA.OperationA();
+            subB.OperationB();
+        }
+
+        public void OperationB()
+        {
+            subB.OperationB();
+            subC.OperationC();
+        }
+    }
+
+    class SubSystemA
+    {
+        public void OperationA()
+        {
+            Console.WriteLine(GetType());
+        }
+    }
+
+    class SubSystemB
+    {
+        public void OperationB()
+        {
+            Console.WriteLine(GetType());
+        }
+    }
+
+    class SubSystemC
+    {
+        public void OperationC()
+        {
+            Console.WriteLine(GetType());
+        }
+    }
+}
