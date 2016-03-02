e29fe97b3f530a8acd2b6d88b92028291f18cd7c
Koleso <fokke_wolf@mail.ru>
2016-03-02 20:34:34 +0300

    1

---
+++ Chain of responsibility/Chain of responsibility/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Chain_of_responsibility
+{
+    class Program
+    {
+        static void Main()
+        {
+            Handler conHandler1 = new ConcreteHandler1();
+            Handler conHandler2 = new ConcreteHandler2();
+
+            conHandler1.Succesor = conHandler2;
+            conHandler1.HandleRequest(3);
+        }
+    }
+
+    abstract class Handler
+    {
+        public Handler Succesor { get; set; }
+        public abstract void HandleRequest(int request);
+    }
+
+    class ConcreteHandler1 : Handler
+    {
+        public override void HandleRequest(int request)
+        {
+            if(request == 1)
+                Console.WriteLine("One");
+            else if(Succesor != null)
+                Succesor.HandleRequest(request);
+        }
+    }
+
+    class ConcreteHandler2 : Handler
+    {
+        public override void HandleRequest(int request)
+        {
+            if ( request == 2 )
+                Console.WriteLine("Two");
+            else if ( Succesor != null )
+                Succesor.HandleRequest(request);
+        }
+    }
+}
