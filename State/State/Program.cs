485f64bf2c890107d7e5ad6352e8ce0a890082e5
Koleso <fokke_wolf@mail.ru>
2016-03-04 19:03:16 +0300

    1

---
+++ State/State/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace State
+{
+    class Program
+    {
+        static void Main()
+        {
+            Context context = new Context(new ConcreteState1());
+
+            Console.WriteLine(context.State); //state 1
+
+            context.Request(); //change state
+            Console.WriteLine(context.State); //state 2
+
+            context.Request(); //change state
+            Console.WriteLine(context.State); //state 1
+        }
+    }
+
+    class Context
+    {
+        public State State { get; set; }
+
+        public Context(State state)
+        {
+            State = state;
+        }
+
+        public void Request()
+        {
+            State.Handle(this);
+        }
+    }
+
+    abstract class State
+    {
+        public abstract void Handle(Context context);
+    }
+
+    class ConcreteState1 : State
+    {
+        public override void Handle(Context context)
+        {
+            context.State = new ConcreteState2();
+        }
+    }
+
+    class ConcreteState2 : State
+    {
+        public override void Handle(Context context)
+        {
+            context.State = new ConcreteState1();
+        }
+    }
+}
