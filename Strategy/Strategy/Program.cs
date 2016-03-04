2069365d1d55fa32993806d849f84343a7d13163
Koleso <fokke_wolf@mail.ru>
2016-03-04 20:26:46 +0300

    1

---
+++ Strategy/Strategy/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Strategy
+{
+    class Program
+    {
+        static void Main()
+        {
+            Context context = new Context(new ConcreteStrategy1());
+            context.ContextInterface(); // Strategy.ConcreteStrategy1
+            
+            context = new Context(new ConcreteStrategy2());
+            context.ContextInterface(); // Strategy.ConcreteStrategy2
+        }
+    }
+
+    class Context
+    {
+        Strategy strategy;
+
+        public Context(Strategy strategy)
+        {
+            this.strategy = strategy;
+        }
+
+        public void ContextInterface()
+        {
+            strategy.AlgoruthmInterface();
+        }
+    }
+
+    abstract class Strategy
+    {
+        public abstract void AlgoruthmInterface();
+    }
+
+    class ConcreteStrategy1 : Strategy
+    {
+        public override void AlgoruthmInterface()
+        {
+            Console.WriteLine(GetType());
+        }
+    }
+
+    class ConcreteStrategy2 : Strategy
+    {
+        public override void AlgoruthmInterface()
+        {
+            Console.WriteLine(GetType());
+        }
+    }
+
+
+}
