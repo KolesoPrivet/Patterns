bad6fb1af252394b3851d01c1e3908d9676180da
Koleso <fokke_wolf@mail.ru>
2016-03-05 14:06:15 +0300

    1

---
+++ Template Method/Template Method/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Template_Method
+{
+    class Program
+    {
+        static void Main()
+        {
+            TwoColorFlag polandFlag = new PolandFlag();
+            polandFlag.Draw();
+        }
+    }
+
+    abstract class TwoColorFlag
+    {
+        public void Draw() //Template method
+        {
+            DrawTopPart();
+            DrawBottomPart();
+            Console.BackgroundColor = ConsoleColor.Black;
+        }
+
+        protected abstract void DrawTopPart();
+        protected abstract void DrawBottomPart();
+    }
+
+    class PolandFlag : TwoColorFlag
+    {
+        protected override void DrawBottomPart()
+        {
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.WriteLine(new string(' ', 7));
+        }
+
+        protected override void DrawTopPart()
+        {
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.WriteLine(new string(' ', 7));
+        }
+    }
+
+    class UkraineFlag : TwoColorFlag
+    {
+        protected override void DrawBottomPart()
+        {
+            Console.BackgroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(new string(' ', 7));
+        }
+
+        protected override void DrawTopPart()
+        {
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.WriteLine(new string(' ', 7));
+        }
+    }
+}
---
+++ Template Method/Template Method/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("Template Method")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Template Method")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("f9ea7ea1-6ff3-4ff3-82fb-81a1af95866c")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
